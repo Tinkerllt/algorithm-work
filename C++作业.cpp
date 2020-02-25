7cde541204bb08ed382f634cf0b9c2542cbc987d
TinkeX123 <llt123456@163.com>
2020-02-25 20:06:13 +0800

    111

--- C++作业.cpp
+++ C++作业.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define Pi 3.14159
 
 int main(){
-    int a,b;
+    int a,b,c;
     cin>>a>>b;
     int x=b%100+b/100*60-(a%100)-a/100*60;
     cout<<setw(2)<<setfill('0')<<x/60<<":"<<setw(2)<<setfill('0')<<x%60<<endl;
